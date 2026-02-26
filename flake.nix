8a415290d85f8222a2bf1e5f5fb131a431e9064e
Tristen Dillon <tristendillon24@outlook.com>
2026-02-25 19:11:11 -0600

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Website dev environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config.allowUnfree = true;
+        };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          name = "website-dev";
+
+          packages = with pkgs; [
+            nodejs_20
+            bun
+            pnpm
+            turbo
+
+            git
+            curl
+            unzip
+            claude-code
+          ];
+        };
+      }
+    );
+}
